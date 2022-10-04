8e6ca3f763c99577f955c75556557ebf134b639a
Imran Ashimov <imranasimov23@gmail.com>
2022-10-04 20:14:03 +0600

    feat:1.10

---
+++ 1.10/script.js
@@ -0,0 +1,17 @@
+const a = 17;
+const b = 10;
+const c = a - b;
+console.log(c);
+const d = 7;
+const result = c + d;
+console.log(result);
+
+
+var today = new Date(10/3/2022);
+var now = today.toLocaleString('ru');
+console.log(now);
+
+var num = 10;
+num = ++num;
+num = ++num;
+alert(num);
