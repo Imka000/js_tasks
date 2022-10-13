0ca987fe65e4bbd7b2a1fbba42ed06c422663f39
Imran Ashimov <imranasimov23@gmail.com>
2022-10-13 20:06:20 +0600

    feat:11.10

---
+++ 11.10/script.js
@@ -0,0 +1,18 @@
+var userSalaries = {
+    Ella: 250,
+    Sophia: 450,
+    Ellie: 500
+};
+console.log(userSalaries.Ella, userSalaries.Sophia);
+
+var keys = {
+    a: 5,
+    b: 6,
+    c: 10
+};
+let result = keys.a + keys.b + keys.c;
+console.log(result);
+
+let number;
+let data = (number === true) ? 3 : 5;
+console.log(data);
