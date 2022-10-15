7b9e68f52095dc3d6cdefa1c63a27fdac7951b30
Imran Ashimov <imranasimov23@gmail.com>
2022-10-15 18:32:57 +0600

    feat:13.10

---
+++ 13.10/script.js
@@ -0,0 +1,22 @@
+let a = [1, 2, 5, 6, 88, 5];
+let result = 0;
+for (let i = 0; i < a.length; i++){
+    result += a[i];
+}
+console.log(result);
+
+
+let x = [10, 20, 30, 70, 520, 140, 150, 123, 124, 125];
+let result2 = 0;
+for(let i = 0; i < x.length; i++){
+    result2 += x[i] * 2;
+}
+console.log(result2);
+
+
+let b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+for (let i = 0; i < b.length; i++){
+    if (b[i] % 2 === 0){
+        console.log(b[i]);
+    }
+}
